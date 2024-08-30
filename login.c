8faf03ecf2060eec8efeb75bada151813dd4a44e
Ella-Gracia <167742778+Ella-Gracia@users.noreply.github.com>
2024-08-30 08:30:30 +0300

    login.c

--- login.c
+++ login.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-void choice();
-void getdata(char** name, char** pwd);
-void save_file(FILE* f, char* name, char* pwd);
-void compare(char* name, char* pwd);
 int main()
 {
     choice();
